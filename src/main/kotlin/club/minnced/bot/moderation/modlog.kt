b9c7c91ab20d392b37a15b43736bc01151d7a057
Florian Spieß <business@minnced.club>
2020-02-10 23:33:14 +0100

    Update copyright

--- src/main/kotlin/club/minnced/bot/moderation/modlog.kt
+++ src/main/kotlin/club/minnced/bot/moderation/modlog.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2019 Florian Spieß
+ *    Copyright 2019-2020 Florian Spieß
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
